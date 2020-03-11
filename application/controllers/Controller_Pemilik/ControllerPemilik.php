d961ae48bb8523acff44e4d3dc6ee9b8ae6c084a
AlbertusNdaru <albertusndarukrismandoko@gmail.com>
2020-03-11 15:19:02 +0700

    Update ControllerPemilik.php

--- application/controllers/Controller_Pemilik/ControllerPemilik.php
+++ application/controllers/Controller_Pemilik/ControllerPemilik.php
@@ -26,6 +26,7 @@ class ControllerPemilik extends CI_Controller
         $nama       = $this->input->post('name');
         $phone      = $this->input->post('phone');
         $Address    = $this->input->post('alamat');
+        
         $JK         = $this->input->post('gender');
         $username   = $this->input->post('username');
         $password   = $this->input->post('password');
