d4f1fd222b20c87412c9d5ab6539cf3857efdea3
AlbertusNdaru <albertusndarukrismandoko@gmail.com>
2020-03-14 21:22:15 +0700

    update template

--- application/views/Template/Template_admin1.php
+++ application/views/Template/Template_admin1.php
@@ -10,48 +10,26 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Rekam Medis Ultima</title>
-    <meta name="description" content="Rekam Medis Ultima">
+    <meta name="description" content="Sufee Admin - HTML5 Admin Template">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <link rel="apple-touch-icon" href="apple-icon.png">
     <link rel="shortcut icon" href="favicon.ico">
 
-
     <link rel="stylesheet" href="<?= base_url('assets/'); ?>vendors/bootstrap/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="<?= base_url('assets/'); ?>vendors/font-awesome/css/font-awesome.min.css">
     <link rel="stylesheet" href="<?= base_url('assets/'); ?>vendors/themify-icons/css/themify-icons.css">
     <link rel="stylesheet" href="<?= base_url('assets/'); ?>vendors/flag-icon-css/css/flag-icon.min.css">
-    <link rel="stylesheet" href="<?= base_url('assets/'); ?>vendors/jqvmap/dist/jqvmap.min.css">
     <link rel="stylesheet" href="<?= base_url('assets/'); ?>vendors/selectFX/css/cs-skin-elastic.css">
-    <link rel="stylesheet" href="<?= base_url('assets/'); ?>vendors/datatables.net-bs4/css/dataTables.bootstrap4.min.css">
-    <link rel="stylesheet" href="<?= base_url('assets/'); ?>vendors/datatables.net-buttons-bs4/css/buttons.bootstrap4.min.css">
+    <link rel="stylesheet" href="<?= base_url('assets/'); ?>vendors/jqvmap/dist/jqvmap.min.css">
+
 
     <link rel="stylesheet" href="<?= base_url('assets/'); ?>assets/css/style.css">
-
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800' rel='stylesheet' type='text/css'>
 
-    <script src="<?= base_url('assets/'); ?>vendors/jquery/dist/jquery.min.js"></script>
-    <script src="<?= base_url('assets/'); ?>vendors/popper.js/dist/umd/popper.min.js"></script>
-    <script src="<?= base_url('assets/'); ?>vendors/bootstrap/dist/js/bootstrap.min.js"></script>
-    <!-- <script src="<?= base_url('assets/'); ?>assets/js/main.js"></script> -->
-
-
-    <script src="<?= base_url('assets/'); ?>vendors/datatables.net/js/jquery.dataTables.min.js"></script>
-    <script src="<?= base_url('assets/'); ?>vendors/datatables.net-bs4/js/dataTables.bootstrap4.min.js"></script>
-    <script src="<?= base_url('assets/'); ?>vendors/datatables.net-buttons/js/dataTables.buttons.min.js"></script>
-    <script src="<?= base_url('assets/'); ?>vendors/datatables.net-buttons-bs4/js/buttons.bootstrap4.min.js"></script>
-    <script src="<?= base_url('assets/'); ?>vendors/jszip/dist/jszip.min.js"></script>
-    <script src="<?= base_url('assets/'); ?>vendors/pdfmake/build/pdfmake.min.js"></script>
-    <script src="<?= base_url('assets/'); ?>vendors/pdfmake/build/vfs_fonts.js"></script>
-    <script src="<?= base_url('assets/'); ?>vendors/datatables.net-buttons/js/buttons.html5.min.js"></script>
-    <script src="<?= base_url('assets/'); ?>vendors/datatables.net-buttons/js/buttons.print.min.js"></script>
-    <script src="<?= base_url('assets/'); ?>vendors/datatables.net-buttons/js/buttons.colVis.min.js"></script>
-    <script src="<?= base_url('assets/'); ?>assets/js/init-scripts/data-table/datatables-init.js"></script>
 </head>
 
 <body>
-    <!-- Left Panel -->
-
     <aside id="left-panel" class="left-panel">
         <nav class="navbar navbar-expand-sm navbar-default">
 
@@ -63,10 +41,48 @@
                 <a class="navbar-brand hidden" href="./"><img src="<?= base_url('assets/'); ?>images/logo2.png" alt="Logo"></a>
             </div>
 
-        <?php include "BarMenu.php" ;?>
-        </nav>
-    </aside><!-- /#left-panel -->
+            <div id="main-menu" class="main-menu collapse navbar-collapse">
+                <ul class="nav navbar-nav">
+                    <li class="active">
+                        <a href="<?= base_url('dashboard') ?>"> <i class="menu-icon fa fa-dashboard"></i>Dashboard </a>
+                    </li>
+                    <!-- /.Kata Page nya akan berubah tergantung siapa yang login berdasarkan level id -->
+                    <h3 class="menu-title">Page Admin</h3>
+                    <li class="active">
+                        <a href="<?= base_url('rekammedis') ?>"> <i class="menu-icon fa fa-dashboard"></i>Rekam Medis </a>
+                        <a href="<?= base_url('pemilik') ?>"> <i class="menu-icon fa fa-dashboard"></i>Pemilik </a>
+                        <a href="<?= base_url('hewan') ?>"> <i class="menu-icon fa fa-dashboard"></i>Hewan </a>
 
+                        <!-- Penentu login admin dan tenaga medis jangan diubah !!-->
+                        <?php if ($_SESSION['Admin']->Id_Level == '1') { ?>
+                            <a href="<?= base_url('tindakan') ?>"> <i class="menu-icon fa fa-dashboard"></i>Tindakan </a>
+                            <a href="<?= base_url('level') ?>"> <i class="menu-icon fa fa-dashboard"></i>Hak Akses </a>
+                            <a href="<?= base_url('tenagamedis') ?>"> <i class="menu-icon fa fa-dashboard"></i>Tenaga Medis </a>
+                            <a href="<?= base_url('penyakit') ?>"> <i class="menu-icon fa fa-dashboard"></i>Penyakit </a>
+                        <?php } ?>
+                    </li>
+                    <h3 class="menu-title">Laporan</h3>
+                    <!-- /.Laporan -->
+                    <li class="active">
+                        <a href="#">Hewan</a>
+                        <a href="#">Rekam Medis</a>
+                        <a href="#">Kritik & Saran</a>
+                    </li>
+                    <h3 class="menu-title">Profile</h3>
+                    <!-- /.Profile -->
+                    <li class="menu-item-has-children dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <i class="menu-icon fa fa-glass"></i>My profile</a>
+                        <ul class="sub-menu children dropdown-menu">
+                            <li><i class="menu-icon fa fa-paper-plane"></i><a href="pages-forget.html">Forget Pass</a></li>
+                            <li><i class="menu-icon fa fa-paper-plane"></i><a href="<?= base_url('ControllerAdmin/Login/logout'); ?>">Logout</a></li>
+                        </ul>
+                    </li>
+
+                </ul>
+            </div><!-- /.navbar-collapse -->
+        </nav>
+    </aside>
     <!-- Left Panel -->
 
     <!-- Right Panel -->
@@ -79,10 +95,8 @@
             <div class="header-menu">
 
 
-            <div class="col-sm-7">
-                    <!-- <a id="menuToggle" class="menutoggle pull-left"><i class="fa fa fa-tasks"></i></a> -->
-                </div>
-                <div class="col-sm-5">
+
+                <div class="col-sm-12">
                     <div class="user-area dropdown float-right">
                         <button href="#" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                             <img class="user-avatar rounded-circle" src="<?= base_url('assets/'); ?>images/admin.jpg" alt="User Avatar">
@@ -90,27 +104,29 @@
                         </button>
 
                         <div class="user-menu dropdown-menu">
-                            <a class="nav-link" href="#"><i class="fa fa-user"></i> My Profile</a>
-                            <a class="nav-link" href="<?= base_url('regadmin') ?>"><i class="fa fa-user"></i> Register</a>
+                            <a class="nav-link" href="<?= base_url('profil'); ?>"><i class="fa fa-user"></i> My Profile</a>
                             <a class="nav-link" href="<?= base_url('ControllerAdmin/Login/logout'); ?>"><i class="fa fa-power-off"></i> Logout</a>
                         </div>
                     </div>
                 </div>
             </div>
         </header><!-- /header -->
-        <!-- Header-->
 
-        <?= $contents?>
+        <div>
+            <div>
 
+                <div>
+                    <?= $contents ?>
+                </div>
+            </div>
+        </div>
 
-    </div><!-- /#right-panel -->
-
-    <!-- Right Panel -->
-
-
-   
-
+       
 
 </body>
+<footer align="center" class="row">
+    <p class="col-md-12 col-sm-12 col-xs-12 copyright">&copy; <a target="_blank">Sistem Informasi Pengelolaan Data Rekam Medis Pet shop & Clinic Ultima</a> <!-- diisi tahun -->
+    </p>
+</footer>
 
 </html>
