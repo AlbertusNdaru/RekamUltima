ef7d1db546eb6fe5c405198fe91f30cedfef4d81
zujajahnurrahmah <43634876+zujajahnurrahmah@users.noreply.github.com>
2020-03-16 20:59:44 +0700

    up

--- application/controllers/Controller_Hewan/ControllerHewan.php
+++ application/controllers/Controller_Hewan/ControllerHewan.php
@@ -15,7 +15,6 @@ class ControllerHewan extends CI_Controller
     function get_hewan()
     {
         $data['hewan'] = $this->Model_hewan->get_hewan();
-
         $this->template->load('Template/Template_admin', 'Form_Hewan/Form_data_Hewan', $data);
     }
 
--- application/views/Landingpage/data_hewan.php
+++ application/views/Landingpage/data_hewan.php
@@ -13,22 +13,30 @@
                 </div>
             </div>
         </div>
+        <div class="row justify-content-right">
+            <div class="col-xl-12">
+                <div class="section_tittle text-right">
+                    <h2>DATA HEWAN</h2>
+                    <!-- <p>Face replenish sea good winged bearing years air divide wasHave night male also</p> -->
+                </div>
+            </div>
+        </div>
         <div class="row" style="margin:auto;">
-        <?php foreach($hewan as $h){ ?>
-            <div class="col-sm-6 col-lg-3">
-                <div class="single_blog_item">
-                    <div class="single_blog_img">
-                        <img src="<?= base_url('assets/landingpage/'); ?>img/doctor/doctor_3.png" alt="doctor">
-                    </div>
-                    <div class="single_text">
-                        <div class="single_blog_text">
-                            <h3><?= $h->Nama_Hewan?></h3>
-                            <p><?= $h->Jenis_Hewan?></p>
+            <?php foreach ($hewan as $h) { ?>
+                <div class="col-sm-6 col-lg-3">
+                    <div class="single_blog_item">
+                        <div class="single_blog_img">
+                            <img src="<?= base_url('assets/landingpage/'); ?>img/doctor/doctor_3.png" alt="doctor">
+                        </div>
+                        <div class="single_text">
+                            <div class="single_blog_text">
+                                <h3><?= $h->Nama_Hewan ?></h3>
+                                <p><?= $h->Jenis_Hewan ?></p>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        <?php } ?>
+            <?php } ?>
         </div>
     </div>
 </section>
