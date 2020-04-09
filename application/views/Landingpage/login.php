d0ed4bfccc46a1c5b39cade2984ad1b69ceee412
AlbertusNdaru <albertusndarukrismandoko@gmail.com>
2020-04-09 09:43:01 +0700

    update notif

--- application/views/Landingpage/login.php
+++ application/views/Landingpage/login.php
@@ -26,6 +26,7 @@
 	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="<?= base_url('assets/loginuser/'); ?>css/util.css">
 	<link rel="stylesheet" type="text/css" href="<?= base_url('assets/loginuser/'); ?>css/main.css">
+	<link  href="<?= base_url('assets/loginuser/'); ?>css/animate.min.css" rel='stylesheet'>
 	<!--===============================================================================================-->
 </head>
 
@@ -42,7 +43,7 @@
 					<span class="login100-form-title p-b-34 p-t-27">
 						Log in
 					</span>
-					<?= $this->session->flashdata('Error'); ?>
+					<!-- <?= $this->session->flashdata('Error'); ?> -->
 					<div class="wrap-input100 validate-input" data-validate="Enter username">
 						<input class="input100" type="text" name="username" placeholder="Username">
 						<span class="focus-input100" data-placeholder="&#xf207;"></span>
@@ -95,7 +96,64 @@
 	<script src="<?= base_url('assets/loginuser/'); ?>vendor/countdowntime/countdowntime.js"></script>
 	<!--===============================================================================================-->
 	<script src="<?= base_url('assets/loginuser/'); ?>js/main.js"></script>
+	<script src="<?= base_url('assets/loginuser/'); ?>js/bootstrap-notify.js"></script>
 
 </body>
+
+<script>
+        <?php if (!empty($this->session->flashdata('Error'))) { ?>
+            setnotif('<?php echo $this->session->flashdata('Error') ?>');
+        <?php } ?>
+
+        function setnotif(err) {
+
+
+            $.notify({
+                // options
+                icon: 'glyphicon glyphicon-warning-sign',
+                title: 'Error',
+                message: err,
+            }, {
+                // settings
+                element: 'body',
+                position: null,
+                type: "danger",
+                allow_dismiss: true,
+                newest_on_top: false,
+                showProgressbar: false,
+                placement: {
+                    from: "top",
+                    align: "center"
+                },
+                offset: 20,
+                spacing: 10,
+                z_index: 1031,
+                delay: 2000,
+                timer: 1000,
+                url_target: '_blank',
+                mouse_over: null,
+                animate: {
+                    enter: 'animated fadeInDown',
+                    exit: 'animated fadeOutUp'
+                },
+                onShow: null,
+                onShown: null,
+                onClose: null,
+                onClosed: null,
+                icon_type: 'class',
+                template: '<div data-notify="container" class="col-xs-11 col-sm-3 alert alert-{0}" role="alert">' +
+                    '<button type="button" aria-hidden="true" class="close" data-notify="dismiss">×</button>' +
+                    '<span data-notify="icon"></span> ' +
+                    '<span data-notify="title">{1}</span> ' +
+                    '<span data-notify="message">{2}</span>' +
+                    '<div class="progress" data-notify="progressbar">' +
+                    '<div class="progress-bar progress-bar-{0}" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%;"></div>' +
+                    '</div>' +
+                    '<a href="{3}" target="{4}" data-notify="url"></a>' +
+                    '</div>'
+            });
+
+        }
+    </script>
 
 </html>
